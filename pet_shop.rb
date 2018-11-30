7f1c736dc0933946f26b7cd16dce08b552f5eae3
user <user@users-MacBook-Pro.local>
2018-11-30 13:40:48 +0000

    cust afford checks complete

--- pet_shop.rb
+++ pet_shop.rb
@@ -123,3 +123,11 @@ end
 def add_pet_to_customer(customer, new_pet)
   customer[:pets].push(new_pet)
 end
+
+def customer_can_afford_pet(customer, new_pet)
+  if customer[:cash] < new_pet[:price]
+    return false
+  else
+    return true
+  end
+end
