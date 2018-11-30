fabc7cb2794f7becda9a090e5eb3d4b171b41a09
user <user@users-MacBook-Pro.local>
2018-11-30 10:42:16 +0000

    total& add remove cash complete

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,11 @@
 def pet_shop_name(pet_shop)
-    return pet_shop[:name] 
+  return pet_shop[:name]
+end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, amount)
+  pet_shop[:admin][:total_cash] += amount
 end
